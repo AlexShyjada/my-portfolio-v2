552ece9d863e4e24aeae2fd1377fac3f53c0c138
Alexandre Shyjada <alexandreshyjada@infojr.com.br>
2021-10-08 01:56:21 -0300

    add header sem responsivo

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,11 +1,54 @@
 <template>
-  <h1>Meu portifólio v2</h1>
+  <Header />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Header from "./components/header/Header.vue";
 
 export default defineComponent({
-
+  name: "App",
+  components: {
+    Header,
+  },
 });
 </script>
+
+<style>
+:root{
+  --white: #fff;
+  --ice: #FAFAFA;
+  --dark-100: #040413;
+  --dark-80: #090918;
+  --blue: #4070F4;
+  --green: #2BD67B;
+}
+
+*{
+  margin: 0;
+  box-sizing: border-box;
+  padding: 0;
+  border: none;
+  background-color: transparent;
+  text-decoration: none;
+  border-radius: 0;
+}
+
+html{
+  font-size: 62.5%;
+  scroll-behavior: smooth;
+}
+
+body{
+  font-family: 'Epilogue', sans-serif;
+  font-size: 1.6rem;
+  background-color: var(--ice)
+}
+
+.container{
+  margin: 0 auto;
+  width: 1235px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ project/src/components/header/Header.vue
@@ -0,0 +1,70 @@
+<template>
+  <header id="Header" class="header">
+    <div class="container">
+      <img src="/assets/logo-AlexShyjada.svg" alt="" />
+      <nav class="nav">
+        <ul class="navLinks">
+          <li><a class="linkHeader" href="#">Inicio</a></li>
+          <li><a class="linkHeader" href="#">Sobre mim</a></li>
+          <li><a class="linkHeader" href="#">Portfólio</a></li>
+          <li><a class="linkHeader" href="#">Darkmode</a></li>
+        </ul>
+      </nav>
+      <button class="btnHeader">Entrar em contato</button>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({})
+</script>
+
+<style scoped>
+
+.header {
+  padding: 2rem;
+  position: fixed;
+  display: flex;
+  width: 100%;
+}
+
+.nav{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.navLinks{
+  list-style: none;
+  display: flex;
+  align-items: center;
+  gap: 5rem;
+}
+
+.linkHeader{
+  display: block;
+  text-align: center;
+  color: var(--dark-100);
+  transition: 0.3s;
+}
+
+.linkHeader:hover{
+  color: var(--green);
+}
+
+.btnHeader{
+  color: var(--white);
+  font-size: 1.4rem;
+  border-radius: 0.5rem;
+  padding: 1.5rem 2rem;
+  background: rgb(43,214,123);
+  background: linear-gradient(45deg, rgba(43,214,123,1) 0%, rgba(64,112,244,1) 100%);
+  transition: 0.3s;
+}
+
+.btnHeader:hover{
+  filter: brightness(0.9);
+}
+
+</style>
